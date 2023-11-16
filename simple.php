e4959cb664f4fbe5fa5fc29ba5465504c9b2e8fa
unknown <triarieffirmansyah30@gmail.com>
2023-11-16 15:23:00 +0700

    commit ke 2

--- simple.php
+++ simple.php
@@ -8,4 +8,11 @@ $client = new Client();
 
 $crawler = $client->request("GET","https://books.toscrape.com/");
 
-echo $crawler->filter('title')->text();
+$file = fopen("data.csv","a");
+
+$crawler->filter('.product_pod')->each (function ($node) use ($file){
+    $title= $node->filter('h3 > a')->attr('title');
+    $price= $node->filter('.price_color')->text();
+    echo $title.'='.$price.PHP_EOL;
+    fputcsv($file,[$title, $price]);
+});
